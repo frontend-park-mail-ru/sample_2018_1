9e6919c45e3dd53475231b649f99eaa289c3da99
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2018-02-09 20:29:01 +0300

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const text = window.document.getElementById('text');
+
+console.log('I am js!', text.innerText);
+
+const button = document.getElementById('button');
+button.addEventListener('mouseout', () => {
+  text.innerText = 'Welcome to frontend';
+})
+
---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const debug = require('debug');
+
+const logger = debug('mylogger')
+
+const server = http.createServer((req, res) => {
+  logger(process.memoryUsage());
+
+  fs.readFile(path.join(__dirname, 'public', req.url), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
+
+    res.write(data);
+    res.end();
+  });
+});
+
+server.listen(3000);
