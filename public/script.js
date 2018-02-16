25233e5a231b5d9e88fcb2dbdc3edfdd7a5c6d73
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2018-02-16 14:38:08 +0300

    Add server

--- public/script.js
+++ public/script.js
@@ -4,6 +4,6 @@ console.log('I am js!', text.innerText);
 
 const button = document.getElementById('button');
 button.addEventListener('mouseout', () => {
-  text.innerText = 'Welcome to frontend';
-})
+	text.innerText = 'Welcome to frontend';
+});
 
