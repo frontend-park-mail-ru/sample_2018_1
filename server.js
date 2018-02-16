57cad9a50cf1911ba77f79936c2ef5ce69c0969e
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2018-02-16 14:21:31 +0300

    Done lesson-1

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const path = require('path');
 const http = require('http');
 const debug = require('debug');
 
-const logger = debug('mylogger')
+const logger = debug('mylogger');
 
 const server = http.createServer((req, res) => {
-  logger(process.memoryUsage());
+	logger(`${req.method} ${req.url}`);
 
-  const filename = req.url === '/' ? 'index.html' : req.url;
+	const filename = req.url === '/' ? 'index.html' : req.url;
 
-  fs.readFile(path.join(__dirname, 'public', filename), (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end();
-      return;
-    }
+	fs.readFile(path.join(__dirname, 'public', filename), (err, data) => {
+		if (err) {
+			res.writeHead(404);
+			res.end();
+			return;
+		}
 
-    res.write(data);
-    res.end();
-  });
+		res.write(data);
+		res.end();
+	});
 });
 
 server.listen(process.env.PORT || 3000);
